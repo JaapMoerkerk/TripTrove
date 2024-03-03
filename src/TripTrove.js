47c0a66a71e9a6ebf06a60c7776b5f7968e4d68a
Jaap Moerkerk <jaapiemoerkerk@hotmail.com>
2024-03-03 18:48:24 +0100

    Full front end overhaul - Darkviolet color - Button styling and hover - Flex container rework - List styling - User input - Title

--- src/TripTrove.js
+++ src/TripTrove.js
@@ -2,7 +2,7 @@ import React, {useState} from "react";
 
 const TripTrove = () => {
     const [inputText, setInputText] = useState("");
-    const [inputList, setInputList] = useState("");
+    const [inputList, setInputList] = useState([]);
 
     const handleInputChange = (event) => {
         setInputText(event.target.value);
@@ -15,37 +15,49 @@ const TripTrove = () => {
         }
     };
 
+    const handleRemoveInput = (index) => {
+        setInputList((prevInput) => prevInput.filter((_, i) => i !== index));
+    }
+
     const handleGenerate = () => {
         console.log("Button clicked")
     };
 
     return (
         <div id={"home-container"}>
-            <div id="user-input-card">
-                <h2>Where are you leading yourself?</h2>
-                <div>
-                    <input
-                        type="text"
-                        value={inputText}
-                        onChange={handleInputChange}
-                        placeholder="Replace with quote API"/>
-                    <button onClick={handleAddInput}>
-                        Add
-                    </button>
-                </div>
-            </div>
+            <h1>Find your new vacation destination with <span className={"title"}>TripTrove</span></h1>
+            <div className={"row"}>
 
-            <div id="input-list">
-                <ul>
-                    {inputList.map((input, index) => (
-                        <li key={index}>{input}</li>
-                    ))}
-                </ul>
-                <button onClick={handleGenerate}>
-                    Generate your next journey
-                </button>
-            </div>
+                <section id="user-input" className={"half-page column"}>
+                    <h2>Where are you leading yourself?</h2>
+                    <div id={"input-div"}>
+                        <input
+                            type="text"
+                            value={inputText}
+                            onChange={handleInputChange}
+                            placeholder="Replace with quote API"/>
+                        <button id={"add-btn"} onClick={handleAddInput}>
+                            Add
+                        </button>
+                    </div>
+                </section>
 
+                <section id="input-list" className={"half-page column"}>
+                    <ul>
+                        <button id={"gen-btn"} onClick={handleGenerate}>
+                            Generate your next journey
+                        </button>
+
+                        {inputList.map((input, index) => (
+                            <li key={index}>
+                                {input}
+                                <button id={"remove-btn"} onClick={() => handleRemoveInput(index)}>Remove</button>
+                            </li>
+                        ))}
+                    </ul>
+                </section>
+
+            </div>
         </div>
     )
 }
